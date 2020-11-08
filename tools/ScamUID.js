3d79ae8ac1f4e6dd1a4e04f5c76ee7bf85f9b9ac
SAHIL <69236315+HELLMAKER0001@users.noreply.github.com>
2020-11-08 15:56:45 +0530

    💖 Creates Random String ( UIDs )

---
+++ tools/ScamUID.js
@@ -0,0 +1,13 @@
+module.exports = {
+    number: (length) => {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+    }
+  };
+  
