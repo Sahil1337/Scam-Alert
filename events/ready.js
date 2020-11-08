317efbdaaf657e205b077073ab84cfc0d7d6e937
SAHIL <69236315+HELLMAKER0001@users.noreply.github.com>
2020-11-08 16:05:02 +0530

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,6 @@
+const { prefix } = require("../Tools/config");
+
+module.exports = async(client) => {
+    await client.user.setActivity(`${prefix}help` , {type: 'STREAMING'}).catch(console.error);
+    console.log("Logged in to discord as" + " " + client.user.username)
+}
