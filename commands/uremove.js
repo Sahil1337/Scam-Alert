09aa0ad30310e8225e8b7f1b41b5a5b27b9e509a
Sudharson <55418697+SudhanPlayz@users.noreply.github.com>
2020-11-08 15:05:06 +0000

    ✨ Pretty Code

--- commands/uremove.js
+++ commands/uremove.js
@@ -1,28 +1,29 @@
-const Scam = require('../Schema/ScamSchema');
-const discord = require('discord.js');
-let UID = require('../Tools/ScamUID.js')
+const Scam = require("../Schema/ScamSchema");
+const discord = require("discord.js");
+let UID = require("../Tools/ScamUID.js");
 
-module.exports.run = async(client, message, args) => {
+module.exports.run = async (client, message, args) => {
+  let element = args[0];
+  if (!element)
+    return message.channel.send(
+      `**Please Provide The Case UID To Remove From ScamList**`
+    );
+  Scam.findOneAndDelete({ CaseUID: element }, (err, data) => {
+    if (err) return message.channel.send(err);
+    if (!data)
+      return message.channel.send(`**No Case Or Scammer Found With This UID**`);
 
-let element = args[0];
-if(!element)return message.channel.send(`**Please Provide The Case UID To Remove From ScamList**`)
-Scam.findOneAndDelete({ CaseUID: element }, (err, data) => {
-   if(err)return message.channel.send(err)
-   if(!data)return message.channel.send(`**No Case Or Scammer Found With This UID**`)
-
-   let embed = new discord.MessageEmbed()
-   .setTitle(`Removed Case From ScamList`)
-   .setColor("RANDOM")
-   .setFooter('ScamAlert | Official')
-   .setDescription(`
+    let embed = new discord.MessageEmbed()
+      .setTitle(`Removed Case From ScamList`)
+      .setColor("RANDOM")
+      .setFooter("ScamAlert | Official").setDescription(`
 __Case UID__ : ${element}
 
-**Now The Case Has Been Removed From The ScamList**`)
-message.channel.send(embed)
-})
-    
-}
+**Now The Case Has Been Removed From The ScamList**`);
+    message.channel.send(embed);
+  });
+};
 module.exports.help = {
-    name: "remove",
-    aliases: ['user-remove']
-}
+  name: "remove",
+  aliases: ["user-remove"],
+};
--- tools/ScamUID.js
+++ tools/ScamUID.js
@@ -1,13 +1,11 @@
 module.exports = {
-    number: (length) => {
+  number: (length) => {
     var result = "";
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
-    }
-  };
-  
+  },
+};
