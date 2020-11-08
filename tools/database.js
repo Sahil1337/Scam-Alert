22e07e904bbde8a6c12708c94d5e75e259bdc010
SAHIL <69236315+HELLMAKER0001@users.noreply.github.com>
2020-11-08 15:55:36 +0530

    Create database.js
    database (mongoose)

---
+++ tools/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("your_mongo_url", {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+mongoose.connection.on('connected',()=>{
+    console.log('[✅ DataBase] Connected!')
+  })
+module.exports = mongoose;
